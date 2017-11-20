778eb390c7c9029f7b92152591dc9d27ba54a508
Wang Peifeng <wangpeifeng@sou.edu.com>
2017-11-20 12:49:11 +0800

    speed up adjustcost  init

--- load_bill.py
+++ load_bill.py
@@ -385,16 +385,26 @@ def main():
     month = config.end_month
     aws_load_bill.delete_month_rows(table=aws_load_bill.config.redshift_t_history, \
                                month=month)
-
-    aws_load_bill.table_copy(table=aws_load_bill.config.redshift_t_history, \
+    calc_key = "s3://" + aws_load_bill.config.proc_bucket + "/" + \
+                      aws_load_bill.config.cal_folder + \
+                      aws_load_bill.config.cal_prefix +month
+    est_key = "s3://" + aws_load_bill.config.proc_bucket + "/" + \
+                      aws_load_bill.config.cal_folder + \
+                      aws_load_bill.config.estimated_prefix +month
+    try:
+        aws_load_bill.table_copy(table=aws_load_bill.config.redshift_t_history, \
                 s3key="s3://" + aws_load_bill.config.proc_bucket + "/" + \
                       aws_load_bill.config.cal_folder + \
                       aws_load_bill.config.cal_prefix +month)
-
-
-
-    #print "\n"
-    aws_load_bill.cli.msg("You got it !  Cheers!")
+    except Exception as err:
+        print(err)
+        aws_load_bill.table_copy(table=aws_load_bill.config.redshift_t_history, \
+                             s3key="s3://" + aws_load_bill.config.proc_bucket + "/" + \
+                                   aws_load_bill.config.cal_folder + \
+                                   aws_load_bill.config.estimated_prefix + month)
+    finally:
+        #print "\n"
+        aws_load_bill.cli.msg("You got it !  Cheers!")
 
 
 if __name__ == '__main__':
